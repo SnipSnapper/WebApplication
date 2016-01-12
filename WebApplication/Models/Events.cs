779c1a5e4fdc2ada98aa2e15d8673cd4aa9ad2dc
Michel <michelv1994@caiway.net>
2016-01-12 09:58:39 +0100

    FAP FAP FAP

---
+++ WebApplication/Models/Events.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models
+{
+    class Events
+    {
+        public ObjectId Id { get; set; }
+        public string DateTime { get; set; }
+        public string UnitID { get; set; }
+        public string Port { get; set; }
+        public string Value { get; set; }
+    }
+}
