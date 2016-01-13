764fd1f586a24acc81ac967f590894c3c9490c75
ceessievdtoren <ceessievdtoren@hotmail.com>
2016-01-13 09:21:22 +0100

    types omgezet

--- WebApplication/Models/DBConnection.cs
+++ WebApplication/Models/DBConnection.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace WebApplication.Models
 {
     class DBConnection
     {
+        static IMongoClient client;
+        static IMongoDatabase database;
+
+
+        public static IMongoDatabase MongoConnection() {
+
+            client = new MongoClient("mongodb://145.24.222.117:27017");
+            database = client.GetDatabase("Planetis");
+
+            return database;
+
+        }
     }
 }
