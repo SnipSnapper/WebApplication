fe2944cec9a4e0e3a7ce4b4159d5c5555ee5172e
ceessievdtoren <ceessievdtoren@hotmail.com>
2016-01-13 16:41:28 +0100

    did a lot of stuff

--- WebApplication/Models/DBQuery.cs
+++ WebApplication/Models/DBQuery.cs
@@ -5,109 +5,86 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using System.Web.UI.WebControls;
 
 namespace WebApplication.Models
 {
     class DBQuery
     {
-        public static List<long> list = new List<long>();
 
-        static void Main(string[] args)
+        static async void Main(string[] args)
         {
             //CallMain(args).Wait();
             //Console.ReadLine();
 
             //MongoDBConnection().Wait();
             //Console.ReadLine();
-
-            Data();
+            
             Console.ReadLine();
         }
 
-        static async Task MongoDBConnection()
-        {
-            var client = new MongoClient("mongodb://145.24.222.117/test2");
-            //var client = new MongoClient("mongodb://localhost:27017");
-            var database = client.GetDatabase("Planetis");
-            var collection = database.GetCollection<BsonDocument>("Positions");
-            var filterBuilder = Builders<BsonDocument>.Filter;
-            var filter = filterBuilder.Gt("Speed", 130) & filterBuilder.Lte("Speed", 160);
+        //public static void Data()
+        //{
+        //    try
+        //    {
+        //        var database = DBConnection.MongoConnection();
+        //        var collection = database.GetCollection<Positions>("positions");
 
-            await collection.Find(filter).ForEachAsync(d => Console.WriteLine(d));
+        //        var query = from c in collection.AsQueryable()
+        //                    where c.Speed == 100
+        //                    select c;
+
+        //        foreach (Positions pos in query)
+        //        {
+        //            //Console.WriteLine(pos.UnitID + " | " + pos.DateTime + " | " + pos.Speed);
+
+        //            long unitId = pos.UnitID;
+        //            list.Add(unitId);
+        //        }
+        //            Console.ReadKey();
+        //    }
+        //    catch (FormatException fe)
+        //    {
+        //        Console.WriteLine("Error: " + fe);
+        //    }
+        //    catch (MongoCommandException mce)
+        //    {
+        //        Console.WriteLine("Error: " + mce);
+        //    }
+        //}
+
+        public static async Task<List<BsonDocument>> GetData() {
+
+            var database = DBConnection.MongoConnection();
+            var collection = database.GetCollection<BsonDocument>("positions");
+
+            var filter = Builders<BsonDocument>.Filter.Eq("Speed", 48);
+
+            var result = await collection.Find(filter).ToListAsync();
+
+            return result;
         }
 
-        static async Task CallMain(string[] args)
+        public static async Task<List<HtmlString>> GetPosition()
         {
-            var Client = new MongoClient("mongodb://localhost:27017");
-            var DB = Client.GetDatabase("Planetis");
-            var collection = DB.GetCollection<BsonDocument>("Positions");
-            //var filterPositions = Builders<BsonDocument>.Filter.Eq("Speed", "0");
 
-            using (var cursor = await collection.Find(new BsonDocument()).ToCursorAsync())
+            var positionList = await Task.Run(() => GetData());
+            List<HtmlString> dataList = new List<HtmlString>();
+            foreach (BsonDocument doc in positionList)
             {
-                while (await cursor.MoveNextAsync())
-                {
-                    foreach (var doc in cursor.Current)
-                    {
-                        Console.WriteLine(doc);
-                    }
-                }
+
+                var value = doc["Speed"];
+                var value2 = doc["DateTime"];
+                var value3 = doc["UnitId"];
+
+
+                var valueResult = new HtmlString(value.ToString() + " | " + value2.ToString() + " | " + value3.ToString());
+                dataList.Add(valueResult);
+
             }
-        }
 
-        public static void Data()
-        {
-            try
-            {
-                var database = DBConnection.MongoConnection();
-                var collection = database.GetCollection<Positions>("positions");
-
-                var query = from c in collection.AsQueryable()
-                            where c.Speed == 100
-                            select c;
-
-
-                foreach (Positions pos in query)
-                {
-                    //Console.WriteLine(pos.UnitID + " | " + pos.DateTime + " | " + pos.Speed);
-
-                    long unitId = pos.UnitID;
-                    list.Add(unitId);
-                }
-                    Console.ReadKey();
-            }
-            catch (FormatException fe)
-            {
-                Console.WriteLine("Error: " + fe);
-            }
-            catch (MongoCommandException mce)
-            {
-                Console.WriteLine("Error: " + mce);
-            }
-        }
-
-        static void getSignalStrenght()
-        {
-            try
-            {
-                MongoClient mc = new MongoClient("mongodb://localhost:27017");
-                var db = mc.GetDatabase("Planetis");
-                var collection = db.GetCollection<Monitoring>("Monitoring");
-                var query = from c in collection.AsQueryable()
-                                //where c.Type == "Hsdpa/RSCP" //&& c.Min == "38"
-                            select c;
-
-                foreach (Monitoring moni in query)
-                {
-                    Console.WriteLine(/*moni.UnitId, moni.BeginTime, moni.Min*/moni);
-                }
-                Console.ReadKey();
-            }
-            catch (FormatException fe)
-            {
-                Console.WriteLine("Error: " + fe);
-            }
+            return dataList;
         }
     }
 }
