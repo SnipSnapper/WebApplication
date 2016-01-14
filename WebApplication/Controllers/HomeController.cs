a0f97a470430dea02077aba60d6040c855630364
Koen <koen.kersbergen@gmail.com>
2016-01-14 11:24:00 +0100

    Google Maps Map and Vector Class

--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -14,6 +14,30 @@ namespace WebApplication.Controllers
             return View();
         }
 
+        public ActionResult GoogleMap()
+        {
+            string d1 = "56.156167";
+            string d2 = "56.256167";
+            Vector2 vec1 = new Vector2(d1, d2);
+            string d3 = "56.356166";
+            Vector2 vec2 = new Vector2(d2, d3);
+            
+
+            List<Vector2> list = new List<Vector2>();
+
+            list.Add(vec1);
+            list.Add(vec2);
+
+            ViewBag.Message = list;
+            return View();
+        }
+
+        public ActionResult Chart()
+        {
+          
+            return View();
+        }
+
         [HttpPost]
         public ActionResult Test1()
         {
---
+++ WebApplication/Models/Vector2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class Vector2
+    {
+        private string lnd;
+        private string ltd;
+
+        public string Lnd
+        {
+            get
+            {
+                return lnd;
+            }
+
+            set
+            {
+                lnd = value;
+            }
+        }
+
+        public string Ltd
+        {
+            get
+            {
+                return ltd;
+            }
+
+            set
+            {
+                ltd = value;
+            }
+        }
+
+        public Vector2(string lnd, string ltd)
+        {
+            this.Lnd = lnd;
+            this.Ltd = ltd;
+        }
+
+        
+
+    }
+}
