0d869c69e4c651271a75d121f25212a930c6d5a2
ceessievdtoren <ceessievdtoren@hotmail.com>
2016-01-18 15:30:01 +0100

    Speed div shows up when speed is selected

--- WebApplication/Scripts/homeScript.js
+++ WebApplication/Scripts/homeScript.js
@@ -2,14 +2,13 @@
 // This code shows the radio buttons for speed when selecting speed out of the dropdown menu.
 
 $(document).ready(function () {
-    $('#att').change(function () {
-        var location = $(this).val(),
-        div = $('#' + location);
-
-        if ($(this).val(location)) {
-            div.show();
-        } else {
-            div.hide();
+    $('#att').on('change', function () {
+        if (this.value == 'Speed')
+        {
+            $("#Speed").show();
+        }
+        else {
+            $("#Speed").hide();
         }
     });
 });
