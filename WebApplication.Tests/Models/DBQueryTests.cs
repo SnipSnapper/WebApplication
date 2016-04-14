8fbeba8ac301d8747b30e80c5054f319f51c9de6
ceessievdtoren <ceessievdtoren@hotmail.com>
2016-04-14 13:55:42 +0200

    added tests that fails

---
+++ WebApplication.Tests/Models/DBQueryTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication.Models.Tests
+{
+    [TestClass()]
+    public class DBQueryTests
+    {
+        [TestMethod()]
+        public void GetSpeedTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetSoftwareTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DateDataTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetSpeedDataTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetUnitIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetSoftwareDataTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetUnitIdDataTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetHardwareTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetHardwareDataTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetDateTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
